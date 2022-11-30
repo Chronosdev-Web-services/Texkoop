37b835e934bd8ac172c2705ef07acbc869ca0455
Bitee-cd <Calebduniya45@gmail.com>
2022-11-30 22:42:13 +0100

    navbar corrections

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <div className={` sticky  z-10 w-full top-0 ${navOpen && "bg-white"}`}>
       {!navOpen && <div className="header_class "></div>}
-      <nav className="max-w-[1440px] w-[90%] flex lg:h-auto flex-col lg:flex-row justify-between py-4 lg:py-7 lg:items-center font-[600] z-10 lg:text-white mx-auto">
+      <nav className="max-w-[1440px] w-[90%] flex lg:h-auto flex-col lg:flex-row  py-4 lg:py-7 lg:items-center font-[600] z-10 lg:text-white mx-auto">
         <div className="flex relative z-20 lg:block items-center w-full lg:w-auto justify-between">
           <div className="">
             <Link href="/">
@@ -58,13 +58,15 @@ const Navbar = () => {
 
         <div
           id="navlinks"
-          className={` flex  flex-col my-7 lg:my-0 gap-10 lg:flex-row justify-between ${
+          className={` flex lg:flex-grow flex-col lg:items-center my-7 lg:my-0 gap-10 lg:flex-row justify-between ${
             navOpen ? "lg:flex z-30 bg-white" : "hidden lg:flex"
           }`}
         >
-          <Link href="/about">
-            <p>{t("Nav1")}</p>
-          </Link>
+          <div className="lg:ml-auto">
+            <Link href="/about">
+              <p>{t("Nav1")}</p>
+            </Link>
+          </div>
           <Link href="/services">
             <p>{t("Nav2")}</p>
           </Link>
@@ -76,33 +78,31 @@ const Navbar = () => {
               <p>{t("Nav4")}</p>
             </Link>
           </div>
-          <div className="">
+          <div className=" lg:ml-auto">
             <Button text={t("Nav5")} />
           </div>
           <div>
-            <ul>
-              <Link
-                href={`${router.asPath}`}
-                locale="en-US"
-                className="flex gap-2 items-center"
-              >
-                <div>
-                  <img src="/icons/gb.svg" alt="English" className="w-[15px]" />
-                </div>
-                En
-              </Link>
+            <Link
+              href={`${router.asPath}`}
+              locale="en-US"
+              className="flex gap-2 items-center"
+            >
+              <div>
+                <img src="/icons/gb.svg" alt="English" className="w-[15px]" />
+              </div>
+              En
+            </Link>
 
-              <Link
-                href={router.asPath}
-                locale="fr"
-                className="flex gap-2 items-center"
-              >
-                <div>
-                  <img src="/icons/fr.svg" alt="french" className="w-[15px] " />
-                </div>
-                <p>Fr</p>
-              </Link>
-            </ul>
+            <Link
+              href={router.asPath}
+              locale="fr"
+              className="flex gap-2 items-center"
+            >
+              <div>
+                <img src="/icons/fr.svg" alt="french" className="w-[15px] " />
+              </div>
+              <p>Fr</p>
+            </Link>
           </div>
         </div>
       </nav>
