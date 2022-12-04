ee9de61dfc5d8c4b904dec404a25c55e1976b368
Bitee-cd <Calebduniya45@gmail.com>
2022-12-04 06:56:35 +0100

    hover button color

--- components/Home/Delivery.js
+++ components/Home/Delivery.js
@@ -25,23 +25,29 @@ const Delivery = () => {
       </section>
       <section className="mx-auto w-[90%] max-w-[1440px] grid md:grid-cols-2 gap-5 my-20">
         {delivery.map((card) => (
-          <div key={card.id} className="h-auto w-full">
-            <Image
-              src={card.image}
-              alt="what we do"
-              className="w-full object-cover"
-              width="100"
-              height="100"
-              priority
-            />
-            <div className="shadow-md px-5 py-3 grid gap-5 hover:bg-sec transition">
+          <div
+            key={card.id}
+            className=" shadow-md hover:bg-sec transition duration-500 card_"
+          >
+            <div>
+              <Image
+                src={card.image}
+                alt="what we do"
+                className="w-full object-cover"
+                width="100"
+                height="100"
+                priority
+              />
+            </div>
+
+            <div className="px-5 py-3 grid gap-5">
               <p className="text-[12px]">{t(card.nav)}</p>
               <Fade cascade>
                 <p className="h4-text font-bold">{t(card.head)}</p>
                 <p className="p-text">{t(card.text)}</p>
               </Fade>
               <div className="flex justify-end">
-                <Button text={t("card_button")} />
+                <Button text={t("card_button")} className=".button" />
               </div>
             </div>
           </div>
